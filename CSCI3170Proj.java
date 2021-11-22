71a34e4a2ab5980e52c9f3d7d98f00c283036832
panliu0310 <liuhoipan@gmail.com>
2021-11-23 01:43:21 +0800

    Added CreateTable, bug not fixed

--- CSCI3170Proj.java
+++ CSCI3170Proj.java
@@ -1,10 +1,44 @@
+import java.io.printStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.Statement;
 import java.sql.SQLException;
 
 public class CSCI3170Proj {
+
+    // Administrator operation 1: Create Table
+    public static void CreateTable(Connection con) {
+        String sql = "Create Table User_Category(
+            ucid Integer PRIMARY KEY,
+            max Integer,
+            period Integer
+        );"
+        Statement stmt = con.createStatement();
+        stmt.executeUpdate(sql);
+    }
+    // Administrator operation 2: Delete Table
+    public static void DeleteTable() {
+
+    }
+    // Administrator operation 3: Load data from a dataset
+    public static void LoadDatafile() {
+
+    }
+    // Administrator operation 4: Show the number of records in each table
+    public static void ShowRecord() {
+
+    }
+    // Administrator operation 5: Return to main menu
+    public static void ReturnToMainMenu() {
+
+    }
+    // show main menu
+    public static void MainMenu() {
+
+    }
+
     public static void main(String[] args) {
-        String dbAddress = "jdbc:mysql://projgw.cse.cuhk.edu.hk:2633/db14";
+        String dbAddress = "jdbc:mysql://projgw.cse.cuhk.edu.hk:2633/db047";
         String dbUsername = "Group14";
         String dbPassword = "3170A";
 
@@ -18,5 +52,7 @@ public class CSCI3170Proj {
         } catch (SQLException e) {
             System.out.printIn(e);
         }
+
+        CreateTable(con);
     }
 }
