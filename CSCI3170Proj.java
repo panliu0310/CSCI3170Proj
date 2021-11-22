836823e9029bc0f6bfbfec66fc6a02291f743933
panliu0310 <liuhoipan@gmail.com>
2021-11-23 00:36:23 +0800

    Tutorial 5

--- CSCI3170Proj.java
+++ CSCI3170Proj.java
@@ -1,29 +1,22 @@
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
-String dbAddress = "jdbc:mysql://projgw.cse.cuhk.edu.hk:2633/db14";
-String dbUsername = "Group14";
-String dbPassword = "3170A";
+public class CSCI3170Proj {
+    public static void main(String[] args) {
+        String dbAddress = "jdbc:mysql://projgw.cse.cuhk.edu.hk:2633/db14";
+        String dbUsername = "Group14";
+        String dbPassword = "3170A";
 
-Connection con = null;
-try{
-    Class.forName("com.mysql.jdbc.Driver");
-    con = DriverManager.getConnection(dbAddress, dbUsername, dbPassword);
-}catch(classNotFoundException e){
-    System.out.println("[Error]: Java MySQL DB Driver not found!");
-    System.exit(0);
-}catch(SQLException e){
-    System.out.println(e);
-    System.exit(0);
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection(dbAddress, dbUsername, dbPassword);
+        } catch (Exception e) {
+            System.out.printIn("[Error]: Java MySQL DB Driver not found!!");
+            System.exit(0);
+        } catch (SQLException e) {
+            System.out.printIn(e);
+        }
+    }
 }
-
-//sql involved Create, Insert, Update, Drop, Delete: stmt.executeUpdate(sql);
-String sql = "SELECT * FROM student 
-                WHERE age = %s and gender = %s;";
-String aUserInputAge = ***; 
-String aUserInputGender = ***;
-sql = String.format(sql, aUserInputAge, aUserInputGender);
-
-Connection con = ***;
-
-Statement stmt = con.createStatement(sql); 
-ResultSet rs = stmt.executeQuery(); //For select query: ResultSet rs = stmt.executeQuery(sql);
