7356f1e7c9582346a3f15df7cf7b83d621e350e5
panliu0310 <liuhoipan@gmail.com>
2021-11-23 21:34:29 +0800

    Connect to Database success

--- CSCI3170Proj.java
+++ CSCI3170Proj.java
@@ -1,4 +1,4 @@
-import java.io.printStream;
+import java.io.PrintStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
@@ -8,13 +8,20 @@ public class CSCI3170Proj {
 
     // Administrator operation 1: Create Table
     public static void CreateTable(Connection con) {
-        String sql = "Create Table User_Category(
-            ucid Integer PRIMARY KEY,
-            max Integer,
-            period Integer
-        );"
-        Statement stmt = con.createStatement();
-        stmt.executeUpdate(sql);
+        String sql = "Create Table User_Category (" +
+            "ucid Integer PRIMARY KEY, " +
+            "max Integer, " +
+            "period Integer)";
+        try {
+            Statement stmt = con.createStatement();
+            stmt.executeUpdate(sql);
+            System.out.println("Table Created!");
+        }catch (SQLException ex){
+            // handle any errors
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        }
     }
     // Administrator operation 2: Delete Table
     public static void DeleteTable() {
@@ -38,21 +45,21 @@ public class CSCI3170Proj {
     }
 
     public static void main(String[] args) {
-        String dbAddress = "jdbc:mysql://projgw.cse.cuhk.edu.hk:2633/db047";
+        String dbAddress = "jdbc:mysql://projgw.cse.cuhk.edu.hk:2633/db14";
         String dbUsername = "Group14";
         String dbPassword = "3170A";
 
         Connection con = null;
         try {
             Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("Connecting to database...");
             con = DriverManager.getConnection(dbAddress, dbUsername, dbPassword);
         } catch (Exception e) {
-            System.out.printIn("[Error]: Java MySQL DB Driver not found!!");
+            System.out.println("[Error]: Java MySQL DB Driver not found!!");
             System.exit(0);
-        } catch (SQLException e) {
-            System.out.printIn(e);
+        } finally {
+            System.out.println("Connected database successfully");
+            CreateTable(con);
         }
-
-        CreateTable(con);
     }
 }
